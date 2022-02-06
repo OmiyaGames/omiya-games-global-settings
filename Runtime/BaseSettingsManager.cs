1f9d84ae1816f63c4b7f13d32a0e296481f5d0a5
Taro Omiya <taro.omiya@omiyagames.com>
2022-02-06 02:58:42 -0600

    Working on setting up a BaseSettingsManager.cs, a base class to interface with BaseSettingsData.

---
+++ Runtime/BaseSettingsManager.cs
@@ -0,0 +1,203 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace OmiyaGames.Global.Settings
+{
+	///-----------------------------------------------------------------------
+	/// <remarks>
+	/// <copyright file="BaseSettingsManager.cs" company="Omiya Games">
+	/// The MIT License (MIT)
+	/// 
+	/// Copyright (c) 2021 Omiya Games
+	/// 
+	/// Permission is hereby granted, free of charge, to any person obtaining a copy
+	/// of this software and associated documentation files (the "Software"), to deal
+	/// in the Software without restriction, including without limitation the rights
+	/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	/// copies of the Software, and to permit persons to whom the Software is
+	/// furnished to do so, subject to the following conditions:
+	/// 
+	/// The above copyright notice and this permission notice shall be included in
+	/// all copies or substantial portions of the Software.
+	/// 
+	/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	/// THE SOFTWARE.
+	/// </copyright>
+	/// <list type="table">
+	/// <listheader>
+	/// <term>Revision</term>
+	/// <description>Description</description>
+	/// </listheader>
+	/// <item>
+	/// <term>
+	/// <strong>Version:</strong> 1.1.0<br/>
+	/// <strong>Date:</strong> 11/27/2021<br/>
+	/// <strong>Author:</strong> Taro Omiya
+	/// </term>
+	/// <description>Initial verison.</description>
+	/// </item>
+	/// </list>
+	/// </remarks>
+	///-----------------------------------------------------------------------
+	/// <summary>
+	/// Abstract class that manages game interaction with
+	/// <typeparamref name="TData"/>.
+	/// </summary>
+	/// <typeparam name="TManager">
+	/// The concrete type extending this class.
+	/// </typeparam>
+	/// <typeparam name="TData">
+	/// The type of stored project settings to read from.
+	/// </typeparam>
+	/// <remarks>
+	/// Most classes extending this one should be composed of static methods
+	/// and properties:
+	/// <code>
+	/// public class TimeManager : BaseSettingsManager&lt;TimeManager, TimeData&gt;
+	/// {
+	///     // Add static methods here
+	/// }
+	/// </code>
+	/// </remarks>
+	public abstract class BaseSettingsManager<TManager, TData> : MonoBehaviour where TManager : BaseSettingsManager<TManager, TData> where TData : BaseSettingsData
+	{
+		/// <summary>
+		/// Indicates the status of whether
+		/// this manager grabbed an instance of 
+		/// <typeparamref name="TData"/>.
+		/// </summary>
+		public enum Status
+		{
+			/// <summary>
+			/// Working on loading the project settings.
+			/// </summary>
+			NowLoading,
+			/// <summary>
+			/// Successfully loaded the project settings.
+			/// </summary>
+			RetrievedProjectData,
+			/// <summary>
+			/// Failed to load the project settings,
+			/// using default settings instead.
+			/// </summary>
+			UsingDefaultData,
+		}
+
+		TData data = null;
+		AsyncOperationHandle<TData> loadDataHandle;
+
+		/// <summary>
+		/// Grabs the static instance of this manager.
+		/// </summary>
+		/// <seealso cref="Data"/>
+		protected static TManager Instance
+		{
+			get
+			{
+				TManager returnInstance = ComponentSingleton<TManager>.Get(out bool isFirstTimeCreated);
+				if (isFirstTimeCreated)
+				{
+					returnInstance.StartCoroutine(returnInstance.OnSetup());
+				}
+				return returnInstance;
+			}
+		}
+
+		/// <summary>
+		/// Grabs the project settings data.
+		/// </summary>
+		/// <seealso cref="Instance"/>
+		/// <seealso cref="IsReady"/>
+		protected static TData Data => Instance.data;
+
+		/// <summary>
+		/// Current status of whether
+		/// data for this manager has been loaded or not
+		/// </summary>
+		public static Status DataStatus
+		{
+			get
+			{
+				switch (Instance.loadDataHandle.Status)
+				{
+					case AsyncOperationStatus.Succeeded:
+						return Status.RetrievedProjectData;
+					case AsyncOperationStatus.Failed:
+						return Status.UsingDefaultData;
+					default:
+						return Status.NowLoading;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Created a coroutine that waits until this manager
+		/// finished loading its data.
+		/// </summary>
+		/// <seealso cref="IsReady"/>
+		public static IEnumerator WaitUntilReady()
+		{
+			TManager check = Instance;
+			while (check.data == null)
+			{
+				yield return null;
+			}
+		}
+
+		/// <summary>
+		/// Gets the path name of data, as an addessable.
+		/// </summary>
+		protected abstract string DataPath
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Event called on creation of this manager
+		/// (before <c>Awake()</c> and <c>Start()</c> are called.)
+		/// </summary>
+		protected virtual IEnumerator OnSetup()
+		{
+			// Attempt to grab a reference to the data
+			loadDataHandle = Addressables.LoadAssetAsync<TData>(DataPath);
+
+			// Wait until it's done loading
+			yield return loadDataHandle;
+
+			// Check the status
+			if (loadDataHandle.Status == AsyncOperationStatus.Succeeded)
+			{
+				// Set data to the results
+				data = loadDataHandle.Result;
+			}
+			else
+			{
+				// Set data to a default instance
+				data = ScriptableObject.CreateInstance<TData>();
+			}
+		}
+
+		/// <summary>
+		/// Event called on disposing this manager.
+		/// </summary>
+		protected virtual void OnDestroy()
+		{
+			// Destroy default data
+			if (DataStatus == Status.UsingDefaultData)
+			{
+				Destroy(data);
+			}
+
+			// Release the handle
+			Addressables.Release(loadDataHandle);
+		}
+	}
+}
